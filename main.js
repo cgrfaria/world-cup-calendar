5a3baac2d5a41bfe4d419fd1951bad2ba0fa0d99
cgrfaria <50423861+cgrfaria@users.noreply.github.com>
2022-11-05 07:47:55 -0300

    intial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira do ${player2}">
+  </li>
+  ` 
+}
+
+let delay = -0.4;
+function createCard(date, day, games) {
+  delay = delay + 0.4;
+  return `
+  <div class="card" style="animation-delay: ${delay}s">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+      ${games}
+    </ul>
+  </div>
+  `
+}
+
+document.querySelector(".cards").innerHTML =
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("england", "10:00", "iran") +
+    createGame("senegal", "13:00", "netherlands") +
+    createGame("united-states", "16:00", "wales")
+  ) +
+  createCard("25/11", "sexta", createGame("qatar", "10:00", "senegal")) +
+  createCard("29/11", "terça", createGame("netherlands", "12:00", "qatar"))
